57905720b864ff63f067b65774e4929a2a8d34a4
Les Harris <les@lesharris.com>
2015-03-15 17:48:56 -0700

    Updated version

--- helm-circe-pkg.el
+++ helm-circe-pkg.el
@@ -1,5 +1,5 @@
 (define-package "helm-circe"
-  "0"
+  "20150315.1"
   "helm circe buffer switching"
   '((emacs "24") (helm "0.0") (circe "0.0") (cl-lib "0.5"))
   :url "https://github.com/lesharris/helm-circe"
