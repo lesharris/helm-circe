3bee655c539b0bb1fc569788d897217e28e50989
Les Harris <les@lesharris.com>
2015-03-16 01:58:51 -0700

    Removed helm-circe-pkg.el

--- helm-circe-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "helm-circe"
-  "20150315.1"
-  "helm circe buffer switching"
-  '((emacs "24") (helm "0.0") (circe "0.0") (cl-lib "0.5"))
-  :url "https://github.com/lesharris/helm-circe"
-  :keywords '("helm" "circe"))
